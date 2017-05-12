ac9efb7a98072c2521724c4c1b7209719cde176a
ENTERRA\reshetnikov <daniil.reshetnikov@enterra-inc.com>
2017-05-12 16:37:58 +0700

    нихуя не работает кароче

--- main.py
+++ main.py
@@ -18,9 +18,6 @@ allies = {}
 worst_heroes_versus = {}
 best_heroes_against = {}
 
-
-
-
 with open('heroes.json') as data_file:
     heroes = json.load(data_file)
 
@@ -28,11 +25,11 @@ with open('heroes.json') as data_file:
 def find_hero(hero_abbr):
     hero_abbr = hero_abbr.lower()
     for hero_name, hero_abbrs in heroes.items():
-        if any(hero_abbr in hero_name for hero_name in hero_abbrs) or (hero_abbr == hero_name):
+        if any(hero_abbr in hero_name for hero_name in hero_abbrs) or (hero_name.find(hero_abbr)):
             return hero_name
 
 
-def makelist(table):
+def make_list_form_table(table):
   result = []
   allrows = table.findAll('tr')
   for row in allrows:
@@ -44,6 +41,27 @@ def makelist(table):
       result[-1].append(thetext)
   return result
 
+
+
+def make_list_for_set(heroes_list):
+    result = []
+    for hero in heroes_list:
+        result.append(hero[1])
+    return result
+
+
+def format_pick_table_html(allies, enemies):
+    table = "<table>    <tr>    <td> goddamit </td>    </tr>    </table>"
+
+    return table
+
+
+# custom_keyboard = [['top-left', 'top-right'], ['bottom-left', 'bottom-right']]
+#
+# reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard)
+# bot.send_message(chat_id=chat_id,
+#                  text="Custom Keyboard Test",
+#                  reply_markup=custom_keyboard)
 
 def get_player_by_name(player_name):
 
@@ -68,6 +86,8 @@ def get_player_by_name(player_name):
     return player
 
 
+
+
 def get_player_best_heroes(player_id):
     url = 'https://ru.dotabuff.com/players/' + str(player_id) + '/heroes'
     headers = {
@@ -79,7 +99,7 @@ def get_player_best_heroes(player_id):
     parsed_page = BeautifulSoup(result_page, 'html.parser')
 
     table = parsed_page.find('table', {'class': 'sortable'})
-    heroes = makelist(table)
+    heroes = make_list_form_table(table)
     heroes.remove([])
 
     best_heroes = []
@@ -88,8 +108,6 @@ def get_player_best_heroes(player_id):
         if float(hero[3].rstrip('%')) > 50:
             hero[1] = re.sub("\d+|[.]", "", hero[1])
             best_heroes.append(hero)
-        if len(best_heroes) == 3:
-            break
 
     return best_heroes
 
@@ -105,7 +123,7 @@ def get_worst_heroes_versus(hero_name):
     parsed_page = BeautifulSoup(result_page, 'html.parser')
 
     table = parsed_page.find('table', {'class': 'sortable'})
-    heroes_versus = makelist(table)
+    heroes_versus = make_list_form_table(table)
     heroes_versus.remove([])
 
     worst_heroes_versus = []
@@ -120,18 +138,29 @@ def get_worst_heroes_versus(hero_name):
     return worst_heroes_versus
 
 
-def calculate_pick(allies = [], enemies = []):
-    result = get_worst_heroes_versus(enemies[0])
+def calculate_pick_statistic(allies = [], enemies = []):
+
+    statistic_heroes = set(make_list_for_set(get_worst_heroes_versus(enemies[0])))
     for enemy in enemies:
-        worst_heroes_versus[enemy] = get_worst_heroes_versus(enemy)
-        print set(result)
-        result = set(worst_heroes_versus[enemy]) and set(result)
+        worst_heroes_versus[enemy] = make_list_for_set(get_worst_heroes_versus(enemy))
+        print statistic_heroes
+        print set(worst_heroes_versus[enemy])
 
-        print result
+        statistic_heroes = set(worst_heroes_versus[enemy]) & statistic_heroes
 
-    return result[0][1]
+        print 'new result:'
+        print statistic_heroes
+        print "\n"
 
-calculate_pick([], ['dazzle', 'arc-warden'])
+
+    return statistic_heroes
+
+
+def calculate_player_best_variants(statistic_heroes, player_best_heroes):
+    return statistic_heroes & set(make_list_for_set(player_best_heroes))
+
+# calculate_pick_statistic([], ['dazzle', 'huskar', 'arc-warden', 'spectre', 'rubick'])
+
 
 @bot.message_handler(commands=['start'])
 def start(message):
@@ -146,8 +175,16 @@ def send_help(message):
     bot.reply_to(message, "Dota 2 counter picker based on your winrate and dotabuff statictics. Send /start to start.")
 
 
-@bot.message_handler(commands=['pick_starts'])
-def pick_starts(message):
+@bot.message_handler(commands=['change_nickname'])
+def change_nickname(message):
+    bot.send_message(message.chat.id, "Okay. Send me your new nickname.")
+    chatStatus[str(message.chat.id)] = 'waiting_for_nickname'
+    enemies[str(message.chat.id)] = []
+    allies[str(message.chat.id)] = []
+
+
+@bot.message_handler(commands=['start_pick'])
+def start_pick(message):
     markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
     markup.add('/ally', '/enemy')
     bot.reply_to(message, 'Who has already picked?', reply_markup=markup)
@@ -167,6 +204,7 @@ def ally_picked(message):
 
 @bot.message_handler()
 def ask_nickname(message):
+    print message.chat.id
     if str(message.chat.id) in chatStatus:
         if chatStatus[str(message.chat.id)] == 'waiting_for_nickname':
             player = get_player_by_name(message.text)
@@ -177,15 +215,22 @@ def ask_nickname(message):
             best_heroes_msg = 'Your best heroes are: \n'
             for hero in best_heroes:
                 best_heroes_msg += hero[1] + ' (' + hero[3] + ') - ' + hero[2] +' matches\n'
+                if len(best_heroes_msg) > 180:
+                    break
             bot.send_message(message.chat.id, best_heroes_msg)
-            bot.send_message(message.chat.id, 'To start pick send /pick_starts')
+
+            markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+            markup.add('/change_nickname', '/start_pick')
+            bot.send_message(message.chat.id, 'Good! Now we can start pick phase.', reply_markup=markup)
 
         if chatStatus[str(message.chat.id)] == 'ally_picked':
             markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
             markup.add('/ally', '/enemy')
             current_hero = find_hero(message.text)
             allies[str(message.chat.id)].append(current_hero)
-            msg = calculate_pick(allies, enemies[str(message.chat.id)])
+            msg = calculate_pick_statistic(allies, enemies[str(message.chat.id)])
+
+
             bot.send_message(message.chat.id, msg , reply_markup=markup)
 
         if chatStatus[str(message.chat.id)] == 'enemy_picked':
@@ -193,7 +238,17 @@ def ask_nickname(message):
             markup.add('/ally', '/enemy')
             current_hero = find_hero(message.text)
             enemies[str(message.chat.id)].append(current_hero)
-            msg = calculate_pick(allies, enemies[str(message.chat.id)])
+
+            # table = format_pick_table_html(allies[str(message.chat.id)], enemies[str(message.chat.id)])
+            #
+            # bot.send_message(message.chat.id, table, parse_mode='HTML')
+
+            msg = "best heroes for this pick will be:" + str(calculate_pick_statistic(allies, enemies[str(message.chat.id)]))
+
+            bot.send_message(message.chat.id, msg, reply_markup=markup)
+
+            msg = "and with you best heroes it can be:" + str(calculate_player_best_variants(calculate_pick_statistic(allies, enemies[str(message.chat.id)]), get_player_best_heroes(133818581)))
+
             bot.send_message(message.chat.id, msg, reply_markup=markup)
 
 
